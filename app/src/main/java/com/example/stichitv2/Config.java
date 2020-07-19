e25442c05d05e42bb1815eb7da1fa31bc5be3f8c
Abdul Rehman Sohail <adrehman11@gmail.com>
2020-07-19 02:59:17 -0700

    done error phase 2

--- app/src/main/java/com/example/stichitv2/Config.java
+++ app/src/main/java/com/example/stichitv2/Config.java
@@ -2,7 +2,7 @@ package com.example.stichitv2;
 //stitchitv2.herokuapp.com
 public class Config {
     static String url = "http://stitchitv2.herokuapp.com/";
-    static String APPID ="19869d9ed6f6f48" ;
+    static String APPID ="21323daecb6ffcd" ;
     static String Region = "us";
-    static String AuthKey = "6fb65ceadc1efb7a0279c268bdd430c952c5d6cf";
+    static String AuthKey = "c7c4e6f76c5f43d197f69947d95d049539c2d978";
 }
